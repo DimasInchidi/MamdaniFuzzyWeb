874d9fc6dab4db2175a73cca27af8bb7ac8188c3
DimasInchidi <Inchidi@yahoo.com>
2015-12-15 23:43:06 +0800

    Update ya kan

--- src/main/java/servlet/F_FB.java
+++ src/main/java/servlet/F_FB.java
@@ -5,9 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * 2015 Dimas Ari for all my friends. Thanks
@@ -16,38 +14,8 @@ import java.util.Map;
 public class F_FB extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String code,url, accessToken, graph;
-    Map fbProfileData;
-    code = "";
-    if (code.equals("")) {
         FBConnection fbConnection = new FBConnection();
         url = fbConnection.getFBAuthUrl();
         response.sendRedirect(url);
-    } else{
-        FBConnection fbConnection = new FBConnection();
-        accessToken = fbConnection.getAccessToken(code);
-        FBGraph fbGraph = new FBGraph(accessToken);
-        graph = fbGraph.getFBGraph();
-        fbProfileData = fbGraph.getGraphData(graph);
-
-        F_Koneksi Koneksi = new F_Koneksi();
-        String query = "SELECT * FROM datauser WHERE userid = '"+fbProfileData.get("id")+"';";
-        if (Koneksi.SelectCheck(query)){
-            HttpSession session = request.getSession();
-            session.setAttribute("ID",fbProfileData.get("id"));
-            response.sendRedirect("/hasil");
-        }else{
-            String fail;
-            HttpSession session = request.getSession();
-            if (session.getAttribute("fail") == null || session.getAttribute("fail").equals("")){
-                fail = "none";
-            } else{
-                fail = session.getAttribute("fail").toString();
-            }
-    }
-}
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
